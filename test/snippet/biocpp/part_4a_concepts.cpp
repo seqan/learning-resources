cc10a676231935c95957e27f2c2e467a7f32ac7f
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-09-10 14:04:52 +0200

    concept snippets ;

---
+++ test/snippet/biocpp/part_4a_concepts.cpp
@@ -0,0 +1,63 @@
+#include <tuple>
+#include <string>
+#include <iostream>
+
+template <typename T>
+concept arithmetic = std::is_arithmetic_v<T>;
+
+static_assert(arithmetic<int>);
+static_assert(arithmetic<float>);
+static_assert(arithmetic<long>);
+// static_assert(arithmetic<std::string>);
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+unsigned add(unsigned a, unsigned b)
+{
+    return a + b;
+}
+
+float add(float a, float b)
+{
+    return a + b;
+}
+
+template <typename T>
+T add_unconstrained(T a, T b) // duck typing
+{
+    // every type that supports the operator+ is valid and compiles
+    return a + b;
+}
+
+template <arithmetic T>
+T add_concept(T a, T b)
+{
+    return a + b;
+}
+
+void add_strings()
+{
+    std::string a{"HELLO "};
+    std::string b{"WORLD"};
+    std::cout << add_unconstrained(a, b) << std::endl;
+    // std::cout << add_concept(a, b) << std::endl; // WILL NOT COMPILE
+}
+
+void add_integrals()
+{
+    int a{2};
+    int b{6};
+
+    std::cout << add_unconstrained(a, b) << std::endl;
+    std::cout << add_concept(a, b) << std::endl;
+}
+
+int main()
+{
+    add_strings();
+    add_integrals();
+    return 0;
+}
