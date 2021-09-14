0df91905aaeb46927486a022085a4ce29f751e4c
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-09-14 12:08:36 +0200

    give structure of indexer

--- src/read_mapper/indexer/store_app_index.hpp
+++ src/read_mapper/indexer/store_app_index.hpp
@@ -7,6 +7,6 @@
 namespace read_mapper::indexer
 {
 
-void store_app_index(read_mapper::indexer::cli_arguments const & arguments, app_index & app_index);
+void store_app_index(read_mapper::indexer::cli_arguments const & arguments, app_index const & app_index);
 
 } // read_mapper::indexer
