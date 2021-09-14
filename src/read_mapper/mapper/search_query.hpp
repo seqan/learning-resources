e6447edca3a349cb350b73b31dbed9e3215447fc
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-09-14 14:16:45 +0200

    give structure of mapper

--- src/read_mapper/mapper/search_query.hpp
+++ src/read_mapper/mapper/search_query.hpp
@@ -12,7 +12,9 @@
 namespace read_mapper::mapper
 {
 
-using search_query_callback = std::function<void(size_t const /*reference_begin_position*/, size_t const /*reference_index*/)>;
+using search_query_callback
+    = std::function<void(size_t const /*reference_begin_position*/,
+                         size_t const /*reference_index*/)>;
 
 void search_query(cli_arguments const & arguments,
                   read_mapper::app_index const & app_index,
